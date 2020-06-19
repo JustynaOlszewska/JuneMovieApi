26c497470b63b4e3a9e3b1b5d3b545a4835d910c
Justyna Olszewka <wioza@wp.pl>
2020-06-19 23:41:48 +0200

    Updates

---
+++ precache-manifest.ea0fa076527f3ae7af0ed3f39e96adf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bd55d9b51ddeece1157",
+    "url": "/JuneMovieApi/static/css/main.7940e5cb.chunk.css"
+  },
+  {
+    "revision": "8bd55d9b51ddeece1157",
+    "url": "/JuneMovieApi/static/js/main.8bd55d9b.chunk.js"
+  },
+  {
+    "revision": "37611993bbeaee52c59f",
+    "url": "/JuneMovieApi/static/js/1.37611993.chunk.js"
+  },
+  {
+    "revision": "ff1c3565635c6e7bba32",
+    "url": "/JuneMovieApi/static/js/runtime~main.ff1c3565.js"
+  },
+  {
+    "revision": "82541ce6becc12f79cdf7d5845b8babd",
+    "url": "/JuneMovieApi/index.html"
+  }
+];
